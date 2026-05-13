87f3c9a3c8abd93be8d8f56c914992270512ab6f
dptmr <github-33@dprelay.de>
2026-05-13 11:56:09 +0200

    added game.run()

--- game.py
+++ game.py
@@ -19,27 +19,61 @@ class Game:
         self.score_font = pygame.font.Font(None, SCORE_FONT_SIZE)
         
     
+    def check_quit_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+    
     def reset(self):
         self.paddle = Paddle()
         self.balls = [Ball()]
         self.score = 0
-    
+        
     def add_ball(self):
         self.balls.append(Ball())
         
     def remove_ball(self, ball):
         self.balls.remove(ball)
         
-    def increase_score(self):
+    def increase_score_and_add_ball(self):
         self.score += 1
-    
-    def get_score(self):
-        return self.score
-    
-    def update_score_text(self):
+        
+    def draw_score(self):
         score_text = self.score_font.render(f"{self.score}", True, SCORE_FONT_COLOR)
         score_text.set_alpha(SCORE_FONT_OPACITY)
         score_box = score_text.get_rect(center=(WINDOW_SIZE / 2, WINDOW_SIZE / 2))
         self.window.blit(score_text, score_box)
         
+    def run(self):
+        while self.running:
+        
+            self.check_quit_event()
+        
+            self.window.fill(BACKGROUND_COLOR)
+
+            self.draw_score()
+    
+            self.paddle.update()
+            self.paddle.draw(self.window)
+    
+            for ball in self.balls[:]:
+                ball.update()
+                ball.draw(self.window)
+        
+                ball.check_window_collision()
+        
+                if ball.check_paddle_collision(self.paddle.hitbox):
+                    self.score += 1
+                    if self.score % 3 == 0:
+                        self.add_ball()
+        
+                if ball.lost():
+                    self.remove_ball(ball)
+            
+                if len(self.balls) == 0:
+                    self.reset()
+        
+            pygame.display.flip()
+        
+            self.clock.tick(FPS)
     
--- main.py
+++ main.py
@@ -1,47 +1,13 @@
 import pygame
 
-from settings import *
 from game import Game
 
 
 def main():
-    
     pygame.init()
     
     game = Game()
-
-    while game.running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game.running = False
-
-        game.window.fill(BACKGROUND_COLOR)
-
-        game.update_score_text()
-    
-        game.paddle.update()
-        game.paddle.draw(game.window)
-    
-        for ball in game.balls[:]:
-            ball.update()
-            ball.draw(game.window)
-        
-            ball.check_window_collision()
-        
-            if ball.check_paddle_collision(game.paddle.hitbox):
-                game.increase_score()
-                if game.get_score() % 3 == 0:
-                    game.add_ball()
-        
-            if ball.lost():
-                game.remove_ball(ball)
-            
-            if len(game.balls) <= 0:
-                game.reset()
-        
-        pygame.display.flip()
-        
-        game.clock.tick(FPS)
+    game.run()
 
     pygame.quit()
     
